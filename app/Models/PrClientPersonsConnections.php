cef3961c4922e6baae9310fea0e6554f5a64c1ca
don79is <don79is@gmail.com>
2017-04-24 15:26:04 +0300

    models name fix

--- app/Models/PrClientPersonsConnections.php
+++ app/Models/PrClientPersonsConnections.php
@@ -6,5 +6,5 @@ class PrClientPersonsConnections extends CoreModel
 {
     protected $table = 'pr_client_persons_connections';
 
-    protected $fillable = ['id','description','client_id','	persons_id','positions_id','comment'];
+    protected $fillable = ['id','client_id','persons_id','positions_id','comment'];
 }
