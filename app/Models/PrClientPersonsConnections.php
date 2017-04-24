cc27f8b3c8634bc44a1ccb60327d7eb362a35103
don79is <don79is@gmail.com>
2017-04-24 14:17:53 +0300

    fix

--- app/Models/PrClientPersonsConnections.php
+++ app/Models/PrClientPersonsConnections.php
@@ -6,5 +6,5 @@ class PrClientPersonsConnections extends CoreModel
 {
     protected $table = 'pr_client_persons_connections';
 
-    protected $fillable = ['id','name','email','phone'];
+    protected $fillable = ['id','description','client_id','	persons_id','positions_id','comment'];
 }
