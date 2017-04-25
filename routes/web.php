647191419099b1727ccd87f5c718ab26c7f96ae5
don79is <don79is@gmail.com>
2017-04-25 11:14:15 +0300

    add barryvdh debugbar

--- routes/web.php
+++ routes/web.php
@@ -1,110 +1,105 @@
 <?php
 
-Route::get('/persons', [
-    'uses' => 'PrPersonsController@index'
+Route::get('/', function () {
+    return view('welcome');
+    });
 
-]);
+Route::group(['prefix' => '/persons'],function () {
 
-Route::get('/generate-fake-data/persons/{count?}', [
-    'uses' => 'FakeDataController@generatePersons'
+    Route::get('/', [
+        'uses' => 'PrPersonsController@index'
+    ]);
+    Route::get('/ProjectTypeConnections', [
+        'uses' => 'PrPersonsProjectTypeConnectionsController@index'
+    ]);
+    Route::get('/Types', [
+        'uses' => 'PrPersonsTypesController@index'
+    ]);
+});
 
-]);
+Route::group(['prefix' => '/client'],function () {
 
+    Route::get('/', [
+        'uses' => 'PrClientController@index'
+    ]);
+    Route::get('/PersonsPositions', [
+        'uses' => 'PrClientPersonsPositionsController@index'
+    ]);
+    Route::get('/PersonsConnections', [
+        'uses' => 'PrClientPersonsConnectionsController@index'
+    ]);
+});
 
-Route::get('/client', [
-    'uses' => 'PrClientController@index'
-
-]);
-
-Route::get('/generate-fake-data/client/{count?}', [
-    'uses' => 'FakeDataController@generateClient'
-
-]);
 
 
 Route::get('/projects', [
     'uses' => 'PrProjectsController@index'
-
 ]);
-
-Route::get('/generate-fake-data/projects/{count?}', [
-    'uses' => 'FakeDataController@generateProjects'
-
-]);
-
-
 Route::get('/projectsTypes', [
     'uses' => 'PrProjectsTypesController@index'
-
 ]);
 
-Route::get('/generate-fake-data/projectsTypes/{count?}', [
-    'uses' => 'FakeDataController@generateProjectsTypes'
 
-]);
 
-Route::get('/clientPersonsPositions', [
-    'uses' => 'PrClientPersonsPositionsController@index'
 
-]);
 
-Route::get('/generate-fake-data/projectsTypes/{count?}', [
-    'uses' => 'FakeDataController@generateClientPersonsPositions'
 
-]);
+
 
 Route::get('/logins', [
     'uses' => 'PrLoginsController@index'
-
 ]);
-
-Route::get('/generate-fake-data/logins/{count?}', [
-    'uses' => 'FakeDataController@generateLogins'
-]);
-
 Route::get('/loginsNames', [
     'uses' => 'PrLoginsNamesController@index'
-
 ]);
-
-Route::get('/generate-fake-data/loginsNames/{count?}', [
-    'uses' => 'FakeDataController@generateLoginsNames'
-]);
-
 Route::get('/loginsProjectsConnections', [
     'uses' => 'PrLoginsProjectsConnectionsController@index'
-
 ]);
 
-Route::get('/generate-fake-data/loginsProjectsConnections/{count?}', [
-    'uses' => 'FakeDataController@generateLoginsProjectsConnections'
-]);
 
-Route::get('/personTypes', [
-    'uses' => 'PrPersonTypesController@index'
 
-]);
 
-Route::get('/generate-fake-data/personTypes/{count?}', [
-    'uses' => 'FakeDataController@generatePersonTypes'
-]);
 
-Route::get('/personsProjectTypeConnections', [
-    'uses' => 'PrPersonsProjectTypeConnectionsController@index'
 
-]);
 
-Route::get('/generate-fake-data/personsProjectTypeConnections/{count?}', [
-    'uses' => 'FakeDataController@generatePersonsProjectTypeConnections'
-]);
 
-Route::get('/clientPersonsConnections', [
-    'uses' => 'PrClientPersonsConnectionsController@index'
 
-]);
+
+
+
 
 Route::get('/generate-fake-data/clientPersonsConnections/{count?}', [
     'uses' => 'FakeDataController@generateClientPersonsConnections'
+]);
+Route::get('/generate-fake-data/persons/{count?}', [
+    'uses' => 'FakeDataController@generatePersons'
+]);
+Route::get('/generate-fake-data/client/{count?}', [
+    'uses' => 'FakeDataController@generateClient'
+]);
+Route::get('/generate-fake-data/personsProjectTypeConnections/{count?}', [
+    'uses' => 'FakeDataController@generatePersonsProjectTypeConnections'
+]);
+Route::get('/generate-fake-data/personsTypes/{count?}', [
+    'uses' => 'FakeDataController@generatePersonsTypes'
+]);
+Route::get('/generate-fake-data/loginsProjectsConnections/{count?}', [
+    'uses' => 'FakeDataController@generateLoginsProjectsConnections'
+]);
+Route::get('/generate-fake-data/loginsNames/{count?}', [
+    'uses' => 'FakeDataController@generateLoginsNames'
+]);
+Route::get('/generate-fake-data/logins/{count?}', [
+    'uses' => 'FakeDataController@generateLogins'
+]);
+Route::get('/generate-fake-data/projectsTypes/{count?}', [
+    'uses' => 'FakeDataController@generateClientPersonsPositions'
+]);
+Route::get('/generate-fake-data/projectsTypes/{count?}', [
+    'uses' => 'FakeDataController@generateProjectsTypes'
+]);
+Route::get('/generate-fake-data/projects/{count?}', [
+    'uses' => 'FakeDataController@generateProjects'
 ]);
 
 
