2eae4b2131ee48d21a3ab60097646ed78ca91e51
don79is <don79is@gmail.com>
2017-04-26 12:20:16 +0300

    created blade files

---
+++ resources/views/meta.blade.php
@@ -0,0 +1,7 @@
+
+
+
+   <meta name="author" content="Donatas Tumanas">
+
+<meta name="description" content="CodeAcademy - Projectx Pr">
+<meta name="keywords" content="laravel, php, framework, web, artisans learning">
